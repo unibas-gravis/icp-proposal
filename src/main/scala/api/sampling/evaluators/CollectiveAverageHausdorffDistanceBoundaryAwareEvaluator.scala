a9a9cbfaf7e2a8a29acd24616bb1681473fec2d2
dennis.madsen <dennis.madsen@unibas.ch>
2021-06-04 09:46:59 +0200

    cleaned up collective average evaluator for efficiency

--- src/main/scala/api/sampling/evaluators/CollectiveAverageHausdorffDistanceBoundaryAwareEvaluator.scala
+++ src/main/scala/api/sampling/evaluators/CollectiveAverageHausdorffDistanceBoundaryAwareEvaluator.scala
@@ -34,43 +34,13 @@ case class CollectiveAverageHausdorffDistanceBoundaryAwareEvaluator(model: Stati
                                                                     numberOfPointsForComparison: Int)(implicit random: Random)
   extends DistributionEvaluator[ModelFittingParameters] with EvaluationCaching {
 
-  def getRandomPointIdsOnModel: IndexedSeq[PointId] = {
-    if (numberOfPointsForComparison >= model.referenceMesh.pointSet.numberOfPoints) {
-      model.referenceMesh.pointSet.pointIds.toIndexedSeq
-    }
-    else {
-      UniformMeshSampler3D(model.referenceMesh, numberOfPointsForComparison).sample().map(_._1)
-        .map(p => model.referenceMesh.pointSet.findClosestPoint(p).id)
-    }
-  }
+  private val decimatedModel = model.decimate(numberOfPointsForComparison)
+  private val decimatedTarget = targetMesh.operations.decimate(numberOfPointsForComparison)
 
-  def getRandomPointsOnTarget: IndexedSeq[Point[_3D]] = {
-    if (numberOfPointsForComparison >= targetMesh.pointSet.numberOfPoints) {
-      targetMesh.pointSet.points.toIndexedSeq
-    }
-    else {
-      UniformMeshSampler3D(targetMesh, numberOfPointsForComparison).sample().map(_._1)
-    }
-  }
+  private val randomPointIdsOnModel = decimatedModel.referenceMesh.pointSet.pointIds.toIndexedSeq
+  private val randomPointsOnTarget = decimatedTarget.pointSet.points.toIndexedSeq
 
-  def hausdorffDistance(m1: TriangleMesh[_3D], m2: TriangleMesh[_3D]): Double = {
-    def allDistsBetweenMeshes(mm1: TriangleMesh[_3D], mm2: TriangleMesh[_3D]): Iterator[Double] = {
-      for (ptM1 <- mm1.pointSet.points) yield {
-        val cpM2 = mm2.operations.closestPointOnSurface(ptM1).point
-        (ptM1 - cpM2).norm
-      }
-    }
-
-    val d1 = allDistsBetweenMeshes(m1, m2)
-    val d2 = allDistsBetweenMeshes(m2, m1)
-
-    Math.max(d1.max, d2.max)
-  }
-
-  private val randomPointIdsOnModel = getRandomPointIdsOnModel
-  private val randomPointsOnTarget = getRandomPointsOnTarget
-
-  def avgDistModelToTarget(modelSample: TriangleMesh[_3D], targetMesh: TriangleMesh[_3D]): (Double, Double) = {
+  def distModelToTarget(modelSample: TriangleMesh[_3D], targetMesh: TriangleMesh[_3D]): (Double, Double) = {
 
     val pointsOnSample = randomPointIdsOnModel.map(modelSample.pointSet.point)
     val dists = for (p <- pointsOnSample) yield {
@@ -79,11 +49,11 @@ case class CollectiveAverageHausdorffDistanceBoundaryAwareEvaluator(model: Stati
       if (targetMesh.operations.pointIsOnBoundary(pTargetId)) -1.0
       else (pTarget - p).norm
     }
-    val filteredDists = dists.toIndexedSeq.filter(f => f > -1.0)
+    val filteredDists = dists.filter(f => f > -1.0)
     (filteredDists.sum / filteredDists.size, filteredDists.max)
   }
 
-  def avgDistTargetToModel(modelSample: TriangleMesh[_3D], targetMesh: TriangleMesh[_3D]): (Double, Double) = {
+  def distTargetToModel(modelSample: TriangleMesh[_3D], targetMesh: TriangleMesh[_3D]): (Double, Double) = {
 
     val dists = for (p <- randomPointsOnTarget) yield {
       val pSample = modelSample.operations.closestPointOnSurface(p).point
@@ -91,18 +61,18 @@ case class CollectiveAverageHausdorffDistanceBoundaryAwareEvaluator(model: Stati
       if (targetMesh.operations.pointIsOnBoundary(pTargetId)) -1.0
       else (pSample - p).norm
     }
-    val filteredDists = dists.toIndexedSeq.filter(f => f > -1.0)
+    val filteredDists = dists.filter(f => f > -1.0)
     (filteredDists.sum / filteredDists.size, filteredDists.max)
   }
 
   def computeLogValue(sample: ModelFittingParameters): Double = {
     val currentSample = ModelFittingParameters.transformedMesh(model, sample)
     val dist = evaluationMode match {
-      case ModelToTargetEvaluation => avgDistModelToTarget(currentSample, targetMesh)
-      case TargetToModelEvaluation => avgDistTargetToModel(currentSample, targetMesh)
+      case ModelToTargetEvaluation => distModelToTarget(currentSample, targetMesh)
+      case TargetToModelEvaluation => distTargetToModel(currentSample, targetMesh)
       case SymmetricEvaluation => {
-        val m2t = avgDistModelToTarget(currentSample, targetMesh)
-        val t2m = avgDistTargetToModel(currentSample, targetMesh)
+        val m2t = distModelToTarget(currentSample, targetMesh)
+        val t2m = distTargetToModel(currentSample, targetMesh)
         (0.5 * m2t._1 + 0.5 * t2m._1, math.max(m2t._2, t2m._2))
       }
     }
